66463dc915f9bfd4c25afbd5cbb34a52956b92f5
sandeep194920 <sandeepamaranath@gmail.com>
2024-01-19 09:07:12 +0530

    books aligned in flex

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -7,7 +7,7 @@ function Books() {
   console.log('The books data is', allBooks)
 
   return (
-    <section>
+    <section className="flex max-w-[90%] gap-20 flex-wrap justify-center md:justify-start my-20 ml-auto">
       {allBooks.map((book) => {
         return <Book key={book.id} {...book} />
       })}
